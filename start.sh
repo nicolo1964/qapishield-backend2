02e62b5e926f3a66709ed028a867efeffb282d78
umairchd <110025544+umairchd@users.noreply.github.com>
2026-08-24 22:51:18 +0500

    Phase 2 (#5)
    * 1- Subscription And Checkout API
    2- Sentry Error and Alerts system
    3- Refresh Token API
    4- Test Suite for the API's
    
    * Removed Local ENV
    
    * Assessments Events Audit logs
    
    * Server Start now includes a proxy header for IP address for a specific device.

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ alembic upgrade head
 
 # Start the FastAPI server
 echo "🌐 Starting FastAPI server..."
-uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000} --proxy-headers --forwarded-allow-ips='*'
